928ae75852529b7174fde9ddb5126dc1147a3e88
Dhruvil <Dhruvil@buildesk.in>
2024-07-16 00:24:02 +0530

    all comps created and add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactUsModule } from './contact-us/contact-us.module';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  }
+    path: 'about-us',
+    loadChildren: () => import('./about-us/about-us.module').then((m) => m.AboutUsModule)
+  },
+  {
+    path: 'services',
+    loadChildren: () => import('./services/services.module').then((m) => m.ServicesModule)
+  },
+  {
+    path: 'menu',
+    loadChildren: () => import('./menu/menu.module').then((m) => m.MenuModule)
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./contact-us/contact-us.module').then((m) => m.ContactUsModule)
+  },
 ];
 
 @NgModule({
