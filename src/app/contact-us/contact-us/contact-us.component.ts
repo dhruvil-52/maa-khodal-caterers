ff5ce55c26c9f4b603ce05eae7a218259ee18f7d
Dhruvil <Dhruvil@buildesk.in>
2024-07-17 02:34:26 +0530

    add google map

--- src/app/contact-us/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us/contact-us.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { user } from 'src/app/shared/user';
+declare var google: any;
 
 @Component({
   selector: 'app-contact-us',
@@ -8,9 +9,47 @@ import { user } from 'src/app/shared/user';
 })
 export class ContactUsComponent implements OnInit {
   user = user;
+  map: any;
+  @ViewChild('map', { static: false }) mapElement: any;
   constructor() { }
+
+  ngAfterViewInit(): void {
+    const mapProperties = {
+      center: new google.maps.LatLng(this.user.lat, this.user.lng),
+      zoom: 15
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
+    new google.maps.Marker({
+      position: new google.maps.LatLng(this.user.lat, this.user.lng),
+      icon: {
+        url: "assets/logos/logo-dark-1.png",
+        scaledSize: new google.maps.Size(60, 40),
+      },
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      draggable: false
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  sendDataToWhatsapp() {
+    var name = "name";
+    var phone = "phone";
+    var email = "email";
+    var service = "service";
+    var url: any = "https://wa.me/918789529215?text=";
+    + "Name: " + name + "%0a"
+      + "Phone: " + phone + "%0a"
+      + "Email: " + email + "%0a"
+      + "Service: " + service;
+    window.open(url, '_blank');
+  }
+
+  openLocationInGoogleMap() {
+    const win: any = window.open('https://www.google.com/maps/search/?api=1&query=' + this.user.lat + ',' + this.user.lng, '_blank');
+    win.focus();
+  }
 }
--- src/app/shared/user.ts
+++ src/app/shared/user.ts
@@ -8,7 +8,7 @@ export const user = {
         },
         {
             name: 'instagram',
-            path: 'assets/icons/instagram.svg',
+            path: 'assets/icons/instagram-logo.svg',
             link: 'https://www.instagram.com/mkc__111/'
         },
         {
@@ -17,9 +17,12 @@ export const user = {
             link: ''
         }
     ],
+    plainAddress: `2,3 Mayur park, Besides Bhagawati School, Hirawadi.`,
     address: `2,3 Mayur park, <br> Besides Bhagawati School, <br> Hirawadi.`,
     mobile: "+91 9876543210",
     mobile2: "+91 9876543210",
     email: "maakhodal@gmail.com",
+    lat: 23.04625066017051,
+    lng: 72.63829799358722,
 
 }
