38476a080bf4c8894c76123e08aac97f20ded320
Dhruvil <Dhruvil@buildesk.in>
2024-07-18 03:16:35 +0530

    home page done

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { user } from 'src/app/shared/user';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  user = user;
+  counter = 0;
+  currentYear = new Date().getFullYear();
+  establishedYear = 1998;
+  totalExperience = 0;
   constructor() { }
 
   ngOnInit(): void {
+    this.totalExperience = (this.currentYear - this.establishedYear)
+    let interval = setInterval(() => {
+      this.counter += 1;
+      if (this.counter == this.totalExperience) {
+        clearInterval(interval);
+      }
+    }, 100);
   }
 
 }
