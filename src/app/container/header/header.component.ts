35916382abdc3587f86f600eb116773d5c698842
Dhruvil <Dhruvil@buildesk.in>
2024-07-27 15:51:01 +0530

    header menu changes

--- src/app/container/header/header.component.ts
+++ src/app/container/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   isMenuOpned: boolean = false;
+
+  constructor(private eRef: ElementRef) { }
+
+  toggleMenu(event: Event) {
+    event.stopPropagation(); // Prevent the click from propagating to the document
+    this.isMenuOpned = !this.isMenuOpned;
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: Event) {
+    if (!this.eRef.nativeElement.contains(event.target)) {
+      this.isMenuOpned = false;
+    }
+  }
 }
